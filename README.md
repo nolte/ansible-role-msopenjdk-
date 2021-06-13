9c57163379469904f6c3df9af72ea0bb7d57f66d
nolte <nolte07@googlemail.com>
2021-06-13 16:33:01 +0200

    init ms openjdk role

---
+++ README.md
@@ -0,0 +1,5 @@
+msopenjdk
+=========
+
+Install and configure [microsoft openjdk](https://docs.microsoft.com/de-de/java/openjdk/install) to host. This role will be download the openjdk archive, and place it to the local filesystem.
+
---
+++ tests/test_default.py
@@ -0,0 +1,13 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+
+def test_java_package_exists(host):
+    cmd = host.run("java --version")
+    assert cmd.rc == 0
+
